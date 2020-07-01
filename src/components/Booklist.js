30909443698b956d4ec3c9891d3676b2fdeb0ebe
Abhiram Reddy <62073792+nrabhiram@users.noreply.github.com>
2020-07-01 20:09:11 +0530

    initial set up

---
+++ src/components/Booklist.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Booklist extends Component {
+    render() { 
+        return ( 
+            <div className="book-list">
+                <ul>
+                    <li>The Way of Kings</li>
+                    <li>The Name of The Wind</li>
+                    <li>The Final Empire</li>
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default Booklist;
